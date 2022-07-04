36a3751a3f62a3520a75a00855429c0bad054bd9
WindingShelf <windingshelf@localhost.localdomain>
2022-07-05 00:46:19 +0530

    second commit

---
+++ build_env/build_scripts/binutils-pass-1.sh
@@ -0,0 +1,19 @@
+cd $LFS/sources
+tar -xf binutils-2.38.tar.xz
+cd binutils-2.38
+
+mkdir -v build
+cd       build
+
+../configure --prefix=$LFS/tools \
+             --with-sysroot=$LFS \
+             --target=$LFS_TGT   \
+             --disable-nls       \
+             --disable-werror
+
+make && make install
+
+cd $LFS/sources
+
+rm -rf binutils-2.38
+
--- build_env/create.sh
+++ build_env/create.sh
@@ -1,6 +1,7 @@
+set -e
+
 echo "Dist Root: ${DIST_ROOT:?}"
 echo "LFS: ${LFS:?}"
-echo "Creating build environment."
 
 if ! test $(whoami) == "distbuild" ; then
     echo "Must run as distbuild!"
@@ -8,3 +9,29 @@ if ! test $(whoami) == "distbuild" ; then
 fi
 
 echo "Creating build environment..."
+cd $DIST_ROOT/build_env
+
+bash -e build_scripts/binutils-pass-1.sh
+bash -e build_scripts/gcc-pass-1.sh
+bash -e build_scripts/linux-headers.sh
+bash -e build_scripts/glibc.sh glibc-2.35.tar.xz
+bash -e build_scripts/libstdcpp-pass-1.sh gcc-11.2.0.tar.xz
+bash -e build_scripts/m4.sh m4-1.4.19.tar.xz
+bash -e build_scripts/ncurses.sh ncurses-6.3.tar.gz
+bash -e build_scripts/bash.sh bash-5.1.16.tar.gz
+bash -e build_scripts/coreutils.sh coreutils-9.0.tar.xz
+bash -e build_scripts/diffutils.sh diffutils-3.8.tar.xz
+bash -e build_scripts/file.sh file-5.41.tar.gz
+bash -e build_scripts/findutils.sh findutils-4.9.0.tar.xz
+bash -e build_scripts/gawk.sh gawk-5.1.1.tar.xz
+bash -e build_scripts/grep.sh grep-3.7.tar.xz
+bash -e build_scripts/gzip.sh gzip-1.11.tar.xz
+bash -e build_scripts/make.sh make-4.3.tar.gz
+bash -e build_scripts/patch.sh patch-2.7.6.tar.xz
+bash -e build_scripts/sed.sh sed-4.8.tar.xz
+bash -e build_scripts/tar.sh tar-1.34.tar.xz
+bash -e build_scripts/xz.sh xz-5.2.5.tar.xz
+bash -e build_scripts/binutils-pass-2.sh
+bash -e build_scripts/gcc-pass-2.sh
+
+echo "DONE!"
